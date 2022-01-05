b04534ea9d5184240db93043f2265d1d60189d44
heemin.seo <heemin.seo@nts-corp.com>
2022-01-05 19:01:24 +0900

    코드 리팩토링
      - commons-cli 사용해서 arguments 받는 방식 수정

--- src/main/java/kr/revelope/study/refactoring/DirtyCodeMain.java
+++ src/main/java/kr/revelope/study/refactoring/DirtyCodeMain.java
@@ -3,7 +3,7 @@ package kr.revelope.study.refactoring;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
-
+import org.apache.commons.cli.*;
 /**
  * csv 파일을 읽어서 특정 컬럼명을 group by 하여 출력하는 프로그램이다.
  * args[0] : resources에 보관된 csv 파일명
@@ -13,12 +13,33 @@ import java.util.stream.Collectors;
  */
 public class DirtyCodeMain {
 	public static void main(String[] args) {
-		if (args == null || args.length < 2) {
+		Options options = new Options();
+
+		Option filePath = new Option("f", "file-path", true, "input file path");
+		filePath.setRequired(true);
+		options.addOption(filePath);
+
+		Option columnName = new Option("c", "column-name", true, "input column name");
+		columnName.setRequired(true);
+		options.addOption(columnName);
+
+		CommandLineParser parser = new DefaultParser();
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLine cmd = null;
+
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			formatter.printHelp("argument-options", options);
+			e.printStackTrace();
+		}
+
+		if (cmd == null) {
 			throw new IllegalArgumentException("File name and target column name is required.");
 		}
 
-		String fileName = args[0];
-		String targetColumnName = args[1];
+		String fileName = cmd.getOptionValue("file-path");
+		String targetColumnName = cmd.getOptionValue("column-name");
 
 		try {
 			FileReader reader = new FileReader(fileName);
